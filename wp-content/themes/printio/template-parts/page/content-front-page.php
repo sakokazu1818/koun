58b665ca225936d4e7d853e4a1e434f1ddd410a3
sako kazuhiro <sako@rio.ne.jp>
2020-08-19 23:40:24 +0900

    リクルートについて作成

--- wp-content/themes/printio/template-parts/page/content-front-page.php
+++ wp-content/themes/printio/template-parts/page/content-front-page.php
@@ -23,26 +23,56 @@
       </ol>
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <img class="d-block main-visual-img" src="wp-content/themes/printio/assets/images/FirstSlide.svg" alt="First slide">
+          <img class="d-block main-visual-img" src="wp-content/themes/printio/assets/images/FirstSlide.png" alt="First slide">
         </div>
         <div class="carousel-item">
-          <img class="d-block main-visual-img" src="wp-content/themes/printio/assets/images/SecondSlide.svg" alt="Second slide">
+          <img class="d-block main-visual-img" src="wp-content/themes/printio/assets/images/SecondSlide.png" alt="Second slide">
         </div>
         <div class="carousel-item">
-          <img class="d-block main-visual-img" src="wp-content/themes/printio/assets/images/ThirdSlide.svg" alt="Third slide">
+          <img class="d-block main-visual-img" src="wp-content/themes/printio/assets/images/ThirdSlide.png" alt="Third slide">
         </div>
         <div class="carousel-item">
-          <img class="d-block main-visual-img" src="wp-content/themes/printio/assets/images/FourthSlide.svg" alt="Fourth slide">
+          <img class="d-block main-visual-img" src="wp-content/themes/printio/assets/images/FourthSlide.png" alt="Fourth slide">
         </div>
         <div class="carousel-item">
-          <img class="d-block main-visual-img" src="wp-content/themes/printio/assets/images/FifthSlide.svg" alt="Fifth slide">
+          <img class="d-block main-visual-img" src="wp-content/themes/printio/assets/images/FifthSlide.png" alt="Fifth slide">
         </div>
       </div>
     </div>
   </section>
 	<section class="c-contents">
-		<div class="l-container01">
-      l-container01
+		<div class="l-container01 inquiry-area">
+      <div class="text-center">
+        <a class="btn btn-primary inquiry-btn" href="#" role="button">
+          <img class="inquiry-img" src="/wp-content/themes/printio/assets/images/logo-2.svg" alt="幸運引越社センターロゴ"></img>
+          <div class="text">引越しのお問い合わせ</div>
+        </a>
+      </div>
     </div>
 	</section>
+  <img class="recurit-img" src="/wp-content/themes/printio/assets/images/staff.svg" alt="リクルート"></img>
+  <section class="c-contents">
+    <div class="l-container01 recurit-info-area">
+      <div class="container c-container">
+        <div class="row">
+          <div class="col-6">
+            <h2>リクルート</h2>
+            <div class="mb-3">お客様は、もちろん一緒に働く社員も同じ。一生お付き合いできる会社です</div>
+            <div class="mb-2">
+              <a class="btn btn-primary more-btn" href="#" role="button">もっと見る</a>
+            </div>
+            <div>
+              <div>
+                <a class="btn btn-primary inquiry-btn" href="#" role="button">リクルートのお問い合わせ</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-6 text-center pl-0 d-flex align-items-end">
+            <img class="recurit-img" src="/wp-content/themes/printio/assets/images/recurit.svg" alt="リクルート"></img>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
 </main>
