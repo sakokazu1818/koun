de710d61c29c7f10ed23c5aec81b2a23d3e8194c
sako kazuhiro <sako@rio.ne.jp>
2020-08-19 09:51:55 +0900

    メインビジュアル実装

--- wp-content/themes/printio/template-parts/page/content-front-page.php
+++ wp-content/themes/printio/template-parts/page/content-front-page.php
@@ -13,9 +13,30 @@
   <section class="p-mainImage">
     <div class="p-mainImage__txt js-pc-p-mainImage__txt"><h1>あなたの引越しトータルサポート</h1></div>
     <div class="p-mainImage__txt js-sp-p-mainImage__txt"><h1>あなたの引越しトータルサポート</h1></div>
-    <div class="swiper-container">
-      <div class="swiper-wrapper">
-        <div class="swiper-slide" style="background-image: url(wp-content/themes/printio/assets/images/parts-photo.svg)"></div>
+    <div id="carousel" class="carousel slide" data-ride="carousel" data-interval="3000">
+      <ol class="carousel-indicators">
+        <li data-target="#carousel" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel" data-slide-to="1"></li>
+        <li data-target="#carousel" data-slide-to="2"></li>
+        <li data-target="#carousel" data-slide-to="3"></li>
+        <li data-target="#carousel" data-slide-to="4"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img class="d-block main-visual-img" src="wp-content/themes/printio/assets/images/parts-photo.svg" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block main-visual-img" src="https://placehold.it/1200x400" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block main-visual-img" src="https://placehold.it/1200x400" alt="Third slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block main-visual-img" src="https://placehold.it/1200x400" alt="Fourth slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block main-visual-img" src="https://placehold.it/1200x400" alt="Fifth slide">
+        </div>
       </div>
     </div>
   </section>
