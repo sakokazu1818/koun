bf5c235019eddde5d50fa1763fa117647a6a68e0
sako kazuhiro <sako@rio.ne.jp>
2020-09-05 23:00:47 +0900

    philosophy

---
+++ wp-content/themes/printio/philosophy.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template Name: philosophy
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since Twenty Seventeen 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<main class="l-body philosophy">
+  <section>
+    <div class="bg-img-1">
+      <div class="title">
+        <h2>企業理念</h2>
+        Corporate Philosophy
+      </div>
+      <div class="content">
+        <div class="content-title">引越を通じて、お客様と一生涯の
+          <br>お付き合いができる関係を 作り上げていく。
+        </div>
+        <p>私たちは、単なる 【 引越センター社】ではなく、引越
+        <p class="pb-15p">は一つのきっかけとしかとらえておりません。
+        <p>一つ一つの出会いを大切にし、お客様の悩み、夢、新生
+        <p>活をトータルサポートすることで生涯お付き合いができ
+        <p>る会社としての想いから作られたのが、この企業理念で
+        <p class="pb-15p">あり、私たちの目的です
+      </div>
+    </div>
+    <div class="bg-img-2"></div>
+  </section>
+</main>
+<?php
+get_footer();
