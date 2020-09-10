e1a01b1eea40babe2c754a1e4676c7568ef9b3a1
sako kazuhiro <sako@rio.ne.jp>
2020-09-11 04:41:18 +0900

    会社情報

---
+++ wp-content/themes/printio/company.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Template Name: company
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since Twenty Seventeen 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<main class="l-body company">
+  <section>
+    <div class="bg-img">
+      <div class="title">
+        <h1>会社情報</h1>
+        Corporate Information
+      </div>
+    </div>
+
+    <table class="table table-striped table-bordered">
+      <tbody>
+        <tr>
+          <th width="160" scope="row">会社名</th>
+          <td>株式会社SELF－PRODUCE</td>
+        </tr>
+        <tr>
+          <th scope="row">会社名≪ﾌﾘｶﾞﾅ≫</th>
+          <td>ｶﾌﾞｼｷｶｲｼｬｾﾙﾌﾌﾟﾛﾃﾞｭｰｽ</td>
+        </tr>
+        <tr>
+          <th scope="row">会社ホームページＵＲＬ</th>
+          <td><a href="https://www.selfproduce.jp/">https://www.selfproduce.jp/</a></td>
+        </tr>
+        <tr>
+          <th scope="row">会社所在地</th>
+          <td>110-0015</td>
+        </tr>
+        <tr>
+          <th scope="row">会社所在地</th>
+          <td>〒110-0015<p>東京都台東区東上野6-2-1<p>MPR東上野ビル6階</td>
+        </tr>
+        <tr>
+          <th scope="row">会社連絡先</th>
+          <td>0120-559-093</td>
+        </tr>
+        <tr>
+          <th scope="row">設立年月日</th>
+          <td>2017年03月31日</td>
+        </tr>
+        <tr>
+          <th scope="row">資本金</th>
+          <td>1,000,000</td>
+        </tr>
+        <tr>
+          <th scope="row">従業員数</th>
+          <td>50名</td>
+        </tr>
+        <tr>
+          <th scope="row">従業員数</th>
+          <td>50名</td>
+        </tr>
+        <tr>
+          <th scope="row">年商</th>
+          <td>300,000,000</td>
+        </tr>
+        <tr>
+          <th scope="row">業種・業態</th>
+          <td>情報通信業事業内容：引越センター</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <div class="bg-img2"></div>
+  </section>
+</main>
+<?php
+get_footer();
