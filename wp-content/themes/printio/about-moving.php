38cace18b4425aecd9dc101f6bda8360893f5b05
sako kazuhiro <sako@rio.ne.jp>
2020-09-17 23:43:49 +0900

    wip

---
+++ wp-content/themes/printio/about-moving.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Template Name: About Moving
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since Twenty Seventeen 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+<main class="l-body about-moving">
+  <section>
+    <div class="bg-img">
+      <div class="title">
+        <h1>引っ越しについて</h1>
+        About Moving
+      </div>
+      <div class="description">
+        お客様の引越しに関わる全てを
+        <p>トータルサポート</p>
+      </div>
+    </div>
+    <div class="container-fluid area-1">
+      <div class="row">
+        <div class="col-1"></div>
+        <div class="col-10">
+          <h2>引越す物件が決まる前から引越し後まで<p>全てを弊社一社でサポート</h2>
+        </div>
+      </div>
+      <div class="row text">
+        <div class="col-1"></div>
+        <div class="col-10">
+          <div>
+            物件検索、初期費用をなるべく抑えたいなどご相談頂ければご相談が可能となります。
+            <p>また、引越後の新居に家具家電を配置した場合のイメージ図を作成して欲しい。</p>
+            <p>家具家電の買い替えを相談したい･･･</p>
+            <p>なんでも弊社までお声掛け下さい。</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="item">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-1"></div>
+          <div class="col-10">
+            <img src="/wp-content/themes/printio/assets/images/aboutmoving-img-1.png" class="img-fluid" alt="引っ越しについて">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-fluid area-2">
+      <div class="row">
+        <div class="col-7">
+          <h2>トータルサポートするから<p>引っ越し費用を安く抑えられる！！</h2>
+          <div class="text">
+            引越後のインフラ（電気ガスインターネット回線の相談）
+            <p>引越後の生活費を見直したい。</p>
+            <p>生涯収益を試算して投資について興味がある･･･</p>
+            <p>などご希望がある方には特別なキャンペーンをご用意させて頂き、一人ひとりに沿ったサービス展開を心がけております。</p>
+          </div>
+        </div>
+        <div class="hoge">
+          <img src="/wp-content/themes/printio/assets/images/aboutmoving-img-2.png" class="item-2" alt="引っ越しについて">
+        </div>
+      </div>
+    </div>
+  </section>
+</main>
+<?php
+get_footer();
