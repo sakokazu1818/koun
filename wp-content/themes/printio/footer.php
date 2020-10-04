872d4abecbe387595c7ecaf1ee2355a0522d1258
sako kazuhiro <sako@rio.ne.jp>
2020-10-04 22:30:20 +0900

    google map 修正

--- wp-content/themes/printio/footer.php
+++ wp-content/themes/printio/footer.php
@@ -15,8 +15,10 @@
 ?>
 <?php wp_footer(); ?>
 <footer class="l-footer">
-  <iframe class="is-sp" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51798.62044172689!2d139.6948194967401!3d35.765210949646765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601892f5059402eb%3A0x1ae9c9853eb9ce72!2z5p2x5Lqs6YO95YyX5Yy6!5e0!3m2!1sja!2sjp!4v1597820084080!5m2!1sja!2sjp" width="100%" height="140px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-  <iframe class="is-pc" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d51798.62044172689!2d139.6948194967401!3d35.765210949646765!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601892f5059402eb%3A0x1ae9c9853eb9ce72!2z5p2x5Lqs6YO95YyX5Yy6!5e0!3m2!1sja!2sjp!4v1597820084080!5m2!1sja!2sjp" width="100%" height="420px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+  <a name='map' >
+    <iframe class="is-sp" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.3521625661533!2d139.69592591464541!3d35.74214808018013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601892b1c63d8a35%3A0x6611a00f2b8180f3!2z44CSMTcxLTAwNDIg5p2x5Lqs6YO96LGK5bO25Yy66auY5p2-77yT5LiB55uu77yR77yS4oiS77yR77yV!5e0!3m2!1sja!2sjp!4v1601818018110!5m2!1sja!2sjp"  width="100%" height="140px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    <iframe class="is-pc" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3238.3521625661533!2d139.69592591464541!3d35.74214808018013!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601892b1c63d8a35%3A0x6611a00f2b8180f3!2z44CSMTcxLTAwNDIg5p2x5Lqs6YO96LGK5bO25Yy66auY5p2-77yT5LiB55uu77yR77yS4oiS77yR77yV!5e0!3m2!1sja!2sjp!4v1601818018110!5m2!1sja!2sjp"  width="100%" height="420px" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+  </a>
   <div class="l-container01 l-footer__container company-info-area">
     <div class="container c-container">
       <div class="row">
@@ -28,9 +30,9 @@
         </div>
 
         <div class="col-4 text-center pl-0 pr-0">
-          <a href="/">
+          <a href="#map">
             <img class="icon" src="/wp-content/themes/printio/assets/images/icon-address.svg" alt="line">
-            <div class="company-info-text">住所住所住所住所住所</div>
+            <div class="company-info-text">東京都豊島区高松3-12-15</div>
           </a>
         </div>
 
